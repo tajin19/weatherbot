46b6bf19e082ec647f6ae790be47d2743d7b3a2e
Tim Metcalf <tjmetcalf@gmail.com>
2016-06-10 14:03:26 -0400

    Temperatures are now being averaged for forcast dates and the dates themselves are correct.

--- README.md
+++ README.md
@@ -13,9 +13,6 @@ What does it do?
   * If you encounter "Use of const in strict mode." during startup, please make sure you have recent version of node.
   * If issues running/reloading make sure you have gulp, nodemon, webpack installed globally.
 
-##### Known Issues/Bugs
-  * When getting 5 day forecast, dates display incorrectly. Identified cause, working on solution.
-
 ##### TO DO
   * Unit tests need implementing.
   * Implement Celsius/Fahrenheit toggle.
--- app/controllers/weatherCtrl.js
+++ app/controllers/weatherCtrl.js
@@ -3,7 +3,7 @@ module.exports = function weatherCtrl($scope, $q, $sce, $state, weatherApiServic
   $scope.currentUnit = 'f';
   $scope.todayShowHeader = false;
   $scope.forecastShowHeader = true;
-  $scope.weatherIndicesFiveDay = [];
+  $scope.weatherIndicesFiveDay = [ [], [], [], [], [] ];
 
   $scope.weatherIndices = $state.params.weatherIndices;
 
@@ -12,7 +12,7 @@ module.exports = function weatherCtrl($scope, $q, $sce, $state, weatherApiServic
     item.forecastOn = false;
   });
 
-  //TODO: Get unit swap working with components, will keep hidden for now
+  //TODO: implement F/C toggle
   var units = {
     'f': {
       name: '&#8457;'
@@ -33,26 +33,63 @@ module.exports = function weatherCtrl($scope, $q, $sce, $state, weatherApiServic
 
   $scope.getFiveDayForcast = function getFiveDayForcast(index){
 
+    //toggle state
     $scope.weatherIndices[index].forecastOn = !$scope.weatherIndices[index].forecastOn;
 
+    //if turning forecast off, delete from records and we are done. no need to call weatherApiService.
     if(!$scope.weatherIndices[index].forecastOn){
       $scope.weatherIndicesFiveDay[index] = [];
       return;
     }
 
-    var cityId = $scope.weatherIndices[index].data.id;
+    var currentCityId = $scope.weatherIndices[index].data.id;
+    var currentDate = new Date($scope.weatherIndices[index].data.dt * 1000);
 
-    weatherApiService.getFiveDayByCityId(cityId)
+    weatherApiService.getFiveDayByCityId(currentCityId)
     .then(function(response){
 
-        $scope.weatherIndicesFiveDay[index] = response.data.list.slice(1,6);
-        $scope.weatherIndicesFiveDay[index].forEach(function(item){
-          //TODO: fix this date issue
-          item.date = new Date(item.dt * 1000).toISOString();
+        //TODO: this function should be broken up for testability
+        var raw = [ [],[],[],[],[],[],[] ];
+        var day = 0;
+
+        //add js date property to each item and group by day
+        response.data.list.forEach(function(item){
+          //create javascript dates
+          item.date = new Date(item.dt * 1000);
+
+          //group temps by day
+          if(raw[day].length > 0 && item.date.getDay() != raw[day][raw[day].length - 1].date.getDay()) {
+            day++;
+          }
+
+          raw[day].push(item);
+        });
+
+        //we are not interested in days where no forecasts are made or our current date
+        raw = raw.filter(function(temps){
+          return temps && temps.length > 0 && temps[0].date.getDay() != currentDate.getDay();
+        });
+
+        raw.forEach(function(item){
+
+          //average numbers for given day
+          var reducedDate = item.reduce(function(prev, curr){
+            curr.main.temp = (curr.main.temp + prev.main.temp) / 2;
+            curr.main.temp_min = (curr.main.temp_min + prev.main.temp_min) / 2;
+            curr.main.temp_max = (curr.main.temp_max + prev.main.temp_max) / 2;
+            curr.main.pressure = (curr.main.pressure + prev.main.pressure) / 2;
+            curr.main.humidity = (curr.main.humidity + prev.main.humidity) / 2;
+            curr.wind.deg = (curr.wind.deg + prev.wind.deg) / 2;
+            curr.wind.speed = (curr.wind.speed + prev.wind.speed) / 2;
+
+            return curr;
+          });
+
+          $scope.weatherIndicesFiveDay[index].push(reducedDate);
         });
 
       }, function(err){
-        //TODO: handle this condition
+          console.log(err);
       });
   };
 };
